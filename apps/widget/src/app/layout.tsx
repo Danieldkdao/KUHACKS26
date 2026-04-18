1cd1dafa7644bab9d37b2a02634d37ec226e2408
Daniel Dao <danieldkdao@gmail.com>
2026-04-18 00:40:38 -0500

    created widget ui

--- apps/widget/src/app/layout.tsx
+++ apps/widget/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Outfit, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const outfitSans = Outfit({
   variable: "--font-outfit-sans",
@@ -28,7 +28,14 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", outfitSans.variable, geistMono.variable, "font-sans", inter.variable)}
+      className={cn(
+        "h-full",
+        "antialiased",
+        outfitSans.className,
+        geistMono.variable,
+        "font-sans",
+        inter.variable,
+      )}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
